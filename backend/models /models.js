b8f2a2c93e3e768e09f35562393f7613f00f2262
Xavier1812 <xavier.paredesloyola@gmail.com>
2025-07-08 17:53:42 -0500

    Primer commit

---
+++ backend/models /models.js
@@ -0,0 +1,6 @@
+export const task = [
+    {id: 1, title:"Tarea1", description: "Es la primera tarea de su familia", completed: true, priority: "2"},
+    {id: 2, title:"Tarea2", description: "Excelente trabajo", completed: false, priority: "3"},
+    {id: 3, title:"Tarea Importante", description: "Esto es de suma importancia", completed: false, priority: "5"},
+];
+
